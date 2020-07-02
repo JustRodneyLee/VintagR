64ea1aea6e4a10474427346a3b29bd00c78f2ea1
JustRodneyLee <1324555186@qq.com>
2020-07-02 15:54:53 +0800

    Add project files.

---
+++ VintagR/VintagR.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace VintagR
+{
+    partial class VintagR
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VintagR));
+            this.VintageButton = new System.Windows.Forms.Button();
+            this.PreviewImage = new System.Windows.Forms.PictureBox();
+            this.LoadImageButton = new System.Windows.Forms.Button();
+            this.listViewImages = new System.Windows.Forms.ListView();
+            this.RemoveImageButton = new System.Windows.Forms.Button();
+            this.OutputDirButton = new System.Windows.Forms.Button();
+            this.StatusStrip = new System.Windows.Forms.StatusStrip();
+            this.StatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.ProgressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.PreviewImage)).BeginInit();
+            this.StatusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // VintageButton
+            // 
+            this.VintageButton.Enabled = false;
+            this.VintageButton.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VintageButton.Location = new System.Drawing.Point(474, 272);
+            this.VintageButton.Name = "VintageButton";
+            this.VintageButton.Size = new System.Drawing.Size(150, 40);
+            this.VintageButton.TabIndex = 0;
+            this.VintageButton.Text = "Process Image";
+            this.VintageButton.UseVisualStyleBackColor = true;
+            this.VintageButton.Click += new System.EventHandler(this.VintageButton_Click);
+            // 
+            // PreviewImage
+            // 
+            this.PreviewImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PreviewImage.Location = new System.Drawing.Point(12, 12);
+            this.PreviewImage.Name = "PreviewImage";
+            this.PreviewImage.Size = new System.Drawing.Size(300, 300);
+            this.PreviewImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PreviewImage.TabIndex = 1;
+            this.PreviewImage.TabStop = false;
+            // 
+            // LoadImageButton
+            // 
+            this.LoadImageButton.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoadImageButton.Location = new System.Drawing.Point(318, 12);
+            this.LoadImageButton.Name = "LoadImageButton";
+            this.LoadImageButton.Size = new System.Drawing.Size(150, 40);
+            this.LoadImageButton.TabIndex = 2;
+            this.LoadImageButton.Text = "Load Images";
+            this.LoadImageButton.UseVisualStyleBackColor = true;
+            this.LoadImageButton.Click += new System.EventHandler(this.LoadImageButton_Click);
+            // 
+            // listViewImages
+            // 
+            this.listViewImages.HideSelection = false;
+            this.listViewImages.Location = new System.Drawing.Point(318, 58);
+            this.listViewImages.Name = "listViewImages";
+            this.listViewImages.Size = new System.Drawing.Size(306, 208);
+            this.listViewImages.TabIndex = 3;
+            this.listViewImages.UseCompatibleStateImageBehavior = false;
+            this.listViewImages.View = System.Windows.Forms.View.List;
+            this.listViewImages.SelectedIndexChanged += new System.EventHandler(this.listViewImages_SelectedIndexChanged);
+            // 
+            // RemoveImageButton
+            // 
+            this.RemoveImageButton.Enabled = false;
+            this.RemoveImageButton.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RemoveImageButton.Location = new System.Drawing.Point(474, 12);
+            this.RemoveImageButton.Name = "RemoveImageButton";
+            this.RemoveImageButton.Size = new System.Drawing.Size(150, 40);
+            this.RemoveImageButton.TabIndex = 4;
+            this.RemoveImageButton.Text = "Remove Image";
+            this.RemoveImageButton.UseVisualStyleBackColor = true;
+            this.RemoveImageButton.Click += new System.EventHandler(this.RemoveImageButton_Click);
+            // 
+            // OutputDirButton
+            // 
+            this.OutputDirButton.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OutputDirButton.Location = new System.Drawing.Point(318, 272);
+            this.OutputDirButton.Name = "OutputDirButton";
+            this.OutputDirButton.Size = new System.Drawing.Size(150, 40);
+            this.OutputDirButton.TabIndex = 5;
+            this.OutputDirButton.Text = "Select Output";
+            this.OutputDirButton.UseVisualStyleBackColor = true;
+            this.OutputDirButton.Click += new System.EventHandler(this.OutputDirButton_Click);
+            // 
+            // StatusStrip
+            // 
+            this.StatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusLabel,
+            this.ProgressBar});
+            this.StatusStrip.Location = new System.Drawing.Point(0, 320);
+            this.StatusStrip.Name = "StatusStrip";
+            this.StatusStrip.Size = new System.Drawing.Size(636, 22);
+            this.StatusStrip.TabIndex = 6;
+            this.StatusStrip.Text = "statusStrip1";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(39, 17);
+            this.StatusLabel.Text = "Ready";
+            // 
+            // ProgressBar
+            // 
+            this.ProgressBar.Name = "ProgressBar";
+            this.ProgressBar.Size = new System.Drawing.Size(100, 16);
+            this.ProgressBar.Visible = false;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "Image Files | *.png;*.jpg;*.jpeg;*.bmp";
+            this.openFileDialog.Multiselect = true;
+            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog_FileOk);
+            // 
+            // VintagR
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 342);
+            this.Controls.Add(this.StatusStrip);
+            this.Controls.Add(this.OutputDirButton);
+            this.Controls.Add(this.RemoveImageButton);
+            this.Controls.Add(this.listViewImages);
+            this.Controls.Add(this.LoadImageButton);
+            this.Controls.Add(this.PreviewImage);
+            this.Controls.Add(this.VintageButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "VintagR";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VintagR";
+            this.Load += new System.EventHandler(this.VintagR_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PreviewImage)).EndInit();
+            this.StatusStrip.ResumeLayout(false);
+            this.StatusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button VintageButton;
+        private System.Windows.Forms.PictureBox PreviewImage;
+        private System.Windows.Forms.Button LoadImageButton;
+        private System.Windows.Forms.ListView listViewImages;
+        private System.Windows.Forms.Button RemoveImageButton;
+        private System.Windows.Forms.Button OutputDirButton;
+        private System.Windows.Forms.StatusStrip StatusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel;
+        private System.Windows.Forms.ToolStripProgressBar ProgressBar;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+    }
+}
+
---
+++ VintagR/VintagR.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Flann;
+
+namespace VintagR
+{
+    public partial class VintagR : Form
+    {
+        Dictionary<string, Image> ImagePreviews;
+        Dictionary<string, Mat> Images;
+        Mat magentaOverlay;
+        const double contrast = 2;
+
+        public VintagR()
+        {
+            InitializeComponent();
+        }
+
+        private void VintagR_Load(object sender, EventArgs e)
+        {
+            ImagePreviews = new Dictionary<string, Image>();
+            Images = new Dictionary<string, Mat>();            
+        }
+
+        private void VintageButton_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.SelectedPath == "")
+            {
+                MessageBox.Show("Please select an output directory!");
+                folderBrowserDialog.ShowDialog();                
+            }
+            else
+            {
+                ProgressBar.Visible = true;
+                ProgressBar.Maximum = Images.Count * 3;
+                ProgressBar.Value = 0;
+                LoadImageButton.Enabled = false;
+                RemoveImageButton.Enabled = false;
+                OutputDirButton.Enabled = false;
+                foreach (KeyValuePair<string, Mat> img in Images)
+                {
+                    StatusLabel.Text = "Vintaging " + img.Key;
+                    MakeImageVintage(img.Key, img.Value);
+                }
+                ProgressBar.Visible = false;
+                LoadImageButton.Enabled = true;
+                RemoveImageButton.Enabled = true;
+                OutputDirButton.Enabled = true;
+                StatusLabel.Text = "Ready";
+            }            
+        }
+
+        private void MakeImageVintage(string name, Mat img)
+        {            
+            int dataSize = (int)(img.Total() * img.Channels());
+            byte[] imgData = new byte[dataSize];
+            byte[] vintageImgData = new byte[dataSize];            
+            Mat vintageImg = new Mat();
+            img.ConvertTo(vintageImg, -1, 1.3, -30); //Contrast and Brightness adjustments
+            var mat3 = new Mat<Vec3b>(vintageImg);
+            var indexer = mat3.GetIndexer();
+            //Cv2.CvtColor(vintageImg, vintageImg, ColorConversionCodes.RGB2HSV); //Convert to HSV
+            Mat hsvImg = new Mat();
+            Cv2.CvtColor(vintageImg, hsvImg, ColorConversionCodes.BGR2HSV_FULL);
+            ProgressBar.Value++;
+            Mat[] hsv = Cv2.Split(hsvImg);
+            hsv[0] -= 5; //Hue change
+            hsv[1] += 15; //Saturation increase
+            hsv[2] += 30; //Lightness increase
+            Cv2.Merge(hsv, hsvImg);
+            Cv2.CvtColor(hsvImg, vintageImg, ColorConversionCodes.HSV2BGR_FULL);
+            ProgressBar.Value++;
+            magentaOverlay = new Mat(img.Rows, img.Cols, img.Type(), new Scalar(144, 0, 255)); //BGR
+            for (int y = 0; y < vintageImg.Height; y++)
+                for (int x = 0; x < vintageImg.Width; x++)
+                {
+                    Vec3b pix = vintageImg.At<Vec3b>(y, x);//BGR
+                    pix.Item0 = (byte)(pix.Item0 * 0.7 + 32);
+                    pix.Item1 = (byte)(-0.0004 * Math.Pow(pix.Item1,2) + 1.1 * pix.Item1 + 0.2);
+                    if (pix.Item2 >= 200)
+                        pix.Item2 = 255;
+                    /*if (pix.Item2 >= (byte)200)
+                        pix.Item2 = (byte)255;
+                    else
+                        pix.Item2 = (byte)(0.0018 * Math.Pow(pix.Item2,2) + 0.9 * x);*/
+                    vintageImg.At<Vec3b>(y, x) = pix;
+                }
+            Cv2.AddWeighted(magentaOverlay, 0.15, vintageImg, 0.85, 0, vintageImg);
+            ProgressBar.Value++;
+            //Cv2.ImShow("Preview", vintageImg);
+            Cv2.ImWrite(folderBrowserDialog.SelectedPath + "/" + name, vintageImg);
+        }
+
+        private void LoadImageButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog.ShowDialog();
+        }
+
+        void AddImage(string path)
+        {
+            string fileName = Path.GetFileName(path);
+            Mat mat = Cv2.ImRead(path);
+            Images.Add(fileName, mat);
+            ImagePreviews.Add(fileName, Image.FromFile(path));                        
+            listViewImages.Items.Add(fileName);
+        }
+
+        void RemoveImage(string name)
+        {
+            Images.Remove(name);
+            ImagePreviews.Remove(name);            
+        }
+
+        private void openFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            ProgressBar.Visible = true;
+            ProgressBar.Maximum = openFileDialog.FileNames.Count();
+            ProgressBar.Value = 0;
+            foreach (string path in openFileDialog.FileNames)
+            {
+                StatusLabel.Text = "Loading image " + path;
+                try
+                {
+                    AddImage(path);                    
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Unable to load image " + path + ":" + ex.ToString());
+                }
+                finally
+                {
+                    ProgressBar.Value++;
+                }
+            }
+            CheckImageCount();
+            StatusLabel.Text = "Ready";
+            ProgressBar.Visible = false;
+        }
+
+        private void listViewImages_SelectedIndexChanged(object sender, EventArgs e)
+        {            
+            if (listViewImages.SelectedItems.Count > 1)
+            {
+                RemoveImageButton.Text = "Remove Images";
+                RemoveImageButton.Enabled = true;
+                PreviewImage.Image = ImagePreviews[listViewImages.SelectedItems[listViewImages.SelectedItems.Count - 1].Text];
+            }                
+            else if (listViewImages.SelectedItems.Count == 0)
+            {
+                RemoveImageButton.Enabled = false;
+                PreviewImage.Image = null;
+            }                
+            else if (listViewImages.SelectedItems.Count == 1)
+            {
+                RemoveImageButton.Text = "Remove Image";
+                RemoveImageButton.Enabled = true;
+                PreviewImage.Image = ImagePreviews[listViewImages.SelectedItems[listViewImages.SelectedItems.Count - 1].Text];
+            }                
+        }
+
+        void CheckImageCount()
+        {
+            if (Images.Count > 1)
+            {
+                VintageButton.Enabled = true;
+                VintageButton.Text = "Process Images";
+            }
+            else if (Images.Count == 1)
+            {
+                VintageButton.Enabled = true;
+                VintageButton.Text = "Process Image";
+            }
+            else
+            {
+                VintageButton.Enabled = false;
+            }
+        }
+
+        void RemoveImage()
+        {
+            List<int> indices = new List<int>();
+            foreach (ListViewItem item in listViewImages.SelectedItems)
+            {
+                Images.Remove(item.Text);
+                ImagePreviews.Remove(item.Text);
+                indices.Add(item.Index);
+            }            
+            foreach (int index in indices)
+            {
+                listViewImages.Items.RemoveAt(index);
+            }            
+            CheckImageCount();
+        }
+
+        private void RemoveImageButton_Click(object sender, EventArgs e)
+        {
+            RemoveImage();
+        }
+
+        private void OutputDirButton_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog.ShowDialog();
+        }
+    }
+}
